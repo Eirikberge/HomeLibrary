b635db242af6d1872e3f4557a213b5a00b280014
Eirik Aspesletten <142005760+Eirikberge@users.noreply.github.com>
2024-04-05 09:34:57 +0200

    .

--- Frontend/src/components/Auth/AuthProvider.jsx
+++ Frontend/src/components/Auth/AuthProvider.jsx
@@ -1,16 +1,9 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState(() => {
-        const storedAuth = localStorage.getItem("auth");
-        return storedAuth ? JSON.parse(storedAuth) : { isAuthenticated: false };
-    });
-
-    useEffect(() => {
-        localStorage.setItem("auth", JSON.stringify(auth));
-    }, [auth]);
+    const [auth, setAuth] = useState({ isAuthenticated: false });
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
